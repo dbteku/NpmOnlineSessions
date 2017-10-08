09bce6868700bc031c1126161a213502016c0261
dbteku <dbteku@hotmail.com>
2017-10-08 00:18:58 -0600

    JSON object or String

--- v1.js
+++ v1.js
@@ -3,6 +3,19 @@ let HttpHelper = require(__dirname + '/lib/HttpHelper');
 const ApiResponse = require(__dirname + '/lib/ApiResponse')
 const HOST = "onlinesessions.io"
 var apiKey;
+
+var test = {
+    test:true
+}
+
+// HttpHelper.httpRequest(HOST, '/v1/createSession?apiKey=test,accountId=test@test.com,time=30s', "POST", undefined, '{"test":true}').then(response => {
+//     console.log(response);
+// });
+
+// HttpHelper.httpRequest(HOST, "/v1/createSession?apiKey=test,sessionId=test@test.com", "GET").then(response => {
+//     console.log(response);
+// });
+
 class OnlineSessions {
     constructor(api) {
         this.api = api;
@@ -10,7 +23,7 @@ class OnlineSessions {
 
     createSession(accountId, sessionData, time, callback) {
         return new Promise(function (resolve, reject) {
-            HttpHelper.httpRequest(HOST, "/createSession?apiKey=" + apiKey + ",accountId=" + accountId + ",time=" + time, "POST", undefined, sessionData).then(response => {
+            HttpHelper.httpRequest(HOST, "/v1/createSession?apiKey=" + apiKey + ",accountId=" + accountId + ",time=" + time, "POST", undefined, sessionData).then(response => {
                 if (callback == undefined) {
                     resolve(response);
                 } else {
@@ -22,7 +35,7 @@ class OnlineSessions {
 
     hasSession(sessionId, callback) {
         return new Promise(function (resolve, reject) {
-            HttpHelper.httpRequest(HOST, "/hasSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "GET").then(response => {
+            HttpHelper.httpRequest(HOST, "/v1/hasSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "GET").then(response => {
                 if (callback == undefined) {
                     resolve(response);
                 } else {
@@ -34,7 +47,7 @@ class OnlineSessions {
 
     getSession(sessionId, callback) {
         return new Promise(function (resolve, reject) {
-            HttpHelper.httpRequest(HOST, "/getSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "GET").then(response => {
+            HttpHelper.httpRequest(HOST, "/v1/getSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "GET").then(response => {
                 if (callback == undefined) {
                     resolve(response);
                 } else {
@@ -46,7 +59,7 @@ class OnlineSessions {
 
     renewSession(sessionId, callback) {
         return new Promise(function (resolve, reject) {
-            HttpHelper.httpRequest(HOST, "/renewSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "GET").then(response => {
+            HttpHelper.httpRequest(HOST, "/v1/renewSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "GET").then(response => {
                 if (callback == undefined) {
                     resolve(response);
                 } else {
@@ -58,7 +71,7 @@ class OnlineSessions {
 
     removeSession(sessionId, callback) {
         return new Promise(function (resolve, reject) {
-            HttpHelper.httpRequest(HOST, "/removeSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "DELETE").then(response => {
+            HttpHelper.httpRequest(HOST, "/v1/removeSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "DELETE").then(response => {
                 if (callback == undefined) {
                     resolve(response);
                 } else {

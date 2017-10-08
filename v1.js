0994e084cd78a6b43ea6f3a37acb56356fc8fcee
dbteku <dbteku@hotmail.com>
2017-10-07 20:59:02 -0600

    v1.0.2

--- v1.js
+++ v1.js
@@ -1,31 +1,75 @@
 'use strict'
 let HttpHelper = require(__dirname + '/lib/HttpHelper');
+const ApiResponse = require(__dirname + '/lib/ApiResponse')
+const HOST = "onlinesessions.io"
 var apiKey;
+class OnlineSessions {
+    constructor(api) {
+        this.api = api;
+    }
 
-function v1(apiKey){
-    this.apiKey = apikey;
+    createSession(accountId, sessionData, time, callback) {
+        HttpHelper.httpRequest(HOST, "/createSession?apiKey=" + apiKey + ",accountId=" + accountId + ",time=" + time, "POST", undefined, sessionData).then(response => {
+            return new Promise(function (resolve, reject) {
+                if (callback == undefined) {
+                    resolve(response);
+                } else {
+                    callback(response);
+                }
+            });
+        });
+    }
+
+    hasSession(sessionId, callback) {
+        HttpHelper.httpRequest(HOST, "/hasSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "GET").then(response => {
+            return new Promise(function (resolve, reject) {
+                if (callback == undefined) {
+                    resolve(response);
+                } else {
+                    callback(response);
+                }
+            });
+        });
+    }
+
+    getSession(sessionId, callback) {
+        HttpHelper.httpRequest(HOST, "/getSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "GET").then(response => {
+            return new Promise(function (resolve, reject) {
+                if (callback == undefined) {
+                    resolve(response);
+                } else {
+                    callback(response);
+                }
+            });
+        });
+    }
+
+    renewSession(sessionId, callback) {
+        HttpHelper.httpRequest(HOST, "/renewSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "GET").then(response => {
+            return new Promise(function (resolve, reject) {
+                if (callback == undefined) {
+                    resolve(response);
+                } else {
+                    callback(response);
+                }
+            });
+        });
+    }
+
+    removeSession(sessionId, callback) {
+        HttpHelper.httpRequest(HOST, "/removeSession?apiKey=" + apiKey + ",sessionId=" + sessionId, "GET").then(response => {
+            return new Promise(function (resolve, reject) {
+                if (callback == undefined) {
+                    resolve(response);
+                } else {
+                    callback(response);
+                }
+            });
+        });
+    }
+
 }
 
-function createSession(accountId, callback){
-    HttpHelper.httpRequest("localhost", "/", "GET", 8192).then(response =>{
-        
-    });
-}
 
-function hasSession(sessionId, callback){
 
-}
-
-function getSession(sessionId, callback){
-    
-}
-
-function renewSession(sessionId, callback){
-    
-}
-
-function removeSession(sessionId, callback){
-    
-}
-
-module.exports = v1;
+module.exports = OnlineSessions;
